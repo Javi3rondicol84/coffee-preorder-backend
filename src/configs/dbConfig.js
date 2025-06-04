58f1ee6f99641c5b3bc8d4032d9164ea3f0886de
javier <javierse9@gmail.com>
2025-06-04 13:34:01 -0300

    removing .env

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -4,15 +4,16 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-export const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
- });
+// export const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false
+//  });
 
+console.log("MYSQL_URL:", process.env.MYSQL_URL);
 
-
-   
-
-
+export const db = new Sequelize(process.env.MYSQL_URL, {
+     dialect: 'mysql',
+     logging: false
+});
 
